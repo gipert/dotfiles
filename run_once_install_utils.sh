f3b2a7aff7d5ffd7524751bcca3f7e3021d7e81e
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2026-03-15 16:04:42 +0100

    Add .config/terminfo/kitty/gruvbox-dark-theme.conf Add .config/terminfo/kitty/kitty.conf Add install_utils.sh Add zshrc.sh

---
+++ run_once_install_utils.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PATH="$PATH:$HOME/.local/bin"
+
+# zsh
+curl -fsLS https://raw.githubusercontent.com/romkatv/zsh-bin/master/install \
+    | sh -s -- -d ~/.local -e no
+
+# zplug
+curl -fsLS https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+# ranger
+python3 -m pip install --user pipx
+pipx run --spec ranger-fm ranger
---
+++ run_onchange_zshrc.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+source ~/.zplug/init.zsh
+
+
