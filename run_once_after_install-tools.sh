e452c3253a8562fa25da57b1b43a29f278e3d963
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2026-03-15 16:10:24 +0100

    Remove .config/terminfo/kitty/gruvbox-dark-theme.conf Remove .config/terminfo/kitty/kitty.conf Remove zshrc.sh Change attributes of install-tools.sh

--- run_once_after_install-tools.sh
+++ run_once_after_install-tools.sh
@@ -9,6 +9,8 @@ curl -fsLS https://raw.githubusercontent.com/romkatv/zsh-bin/master/install \
 # zplug
 curl -fsLS https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
+source ~/.zplug/init.zsh
+
 # ranger
 python3 -m pip install --user pipx
 pipx run --spec ranger-fm ranger
--- run_onchange_zshrc.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-
-source ~/.zplug/init.zsh
-
-
