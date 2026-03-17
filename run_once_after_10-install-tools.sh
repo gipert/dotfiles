4e8221c5fec6b398ad742f54842f16c5debd4c0b
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2026-03-17 21:29:14 +0100

    do not install stuff if already present

--- run_once_after_10-install-tools.sh
+++ run_once_after_10-install-tools.sh
@@ -13,8 +13,10 @@ curl -fsSL https://raw.githubusercontent.com/kovidgoyal/kitty/master/terminfo/ki
 
 # zsh
 echo "INFO: installing zsh"
-curl -fsLS https://raw.githubusercontent.com/romkatv/zsh-bin/master/install \
-    | sh -s -- -d $INSTALL -e no
+if ! command -v zsh > /dev/null; then
+    curl -fsLS https://raw.githubusercontent.com/romkatv/zsh-bin/master/install \
+        | sh -s -- -d $INSTALL -e no
+fi
 
 # zplug
 echo "INFO: installing zplug and plugins"
@@ -23,15 +25,18 @@ if [ ! -f ~/.zplug/init.zsh ]; then
     # HACK: if we don't wait, we get a 'no such file or directory' when installing plugins
     sleep 5
 fi
+
 zsh -ic 'zplug install'
 
 # ranger
-echo "INFO: installing ranger-fm"
-python3 -m venv $INSTALL/opt/ranger
-$INSTALL/opt/ranger/bin/python -m pip install -U pip
-$INSTALL/opt/ranger/bin/python -m pip install ranger-fm
+if ! command -v ranger > /dev/null; then
+    echo "INFO: installing ranger-fm"
+    python3 -m venv $INSTALL/opt/ranger
+    $INSTALL/opt/ranger/bin/python -m pip install -U pip
+    $INSTALL/opt/ranger/bin/python -m pip install ranger-fm
 
-mkdir -p $INSTALL/bin
-ln -fs $INSTALL/opt/ranger/bin/ranger $INSTALL/bin/ranger
+    mkdir -p $INSTALL/bin
+    ln -fs $INSTALL/opt/ranger/bin/ranger $INSTALL/bin/ranger
+fi
 
 exit 0
